199045ecc9eed9567f5b5500dace6ae2d8275f44
yangyueguang <2829969299@qq.com>
2021-08-10 22:36:49 +0800

    delete some

--- react/app/pages/menu/index.js
+++
@@ -1,8 +0,0 @@
-
-import echarts from './echarts'
-import editor from './editor'
-import chat from './chat'
-
-export {
-  echarts, editor, chat,
-}
--- vue/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-image_url=yangyueguang/vue:20200808
-
-sudo docker build --build-arg https_proxy="${https_proxy}" -t $image_url .
-sudo docker push $image_url
-
-docker-compose up -d
